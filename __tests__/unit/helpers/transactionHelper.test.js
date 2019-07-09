82e6884b82af598e5ad814f7e7fd71c2fdd8ed01
vtorres96 <victorcdc96@gmail.com>
2019-07-09 17:11:20 -0300

    creating unit tests

---
+++ __tests__/unit/helpers/transactionHelper.test.js
@@ -0,0 +1,12 @@
+const { getLastFourNumbers } = require('../../../src/helpers/transactionHelper')
+
+describe('Transaction Helper', () => {
+
+    it('should be able to capture the last four numbers', () => {
+      const cardNumber = 1234567891234567
+     
+      const response = getLastFourNumbers(cardNumber)
+  
+      expect(response).toEqual(4567)
+    })
+})

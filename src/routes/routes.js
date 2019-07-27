49125ddb58d1868311fe760de4c372315e820ad2
vtorres96 <victorcdc96@gmail.com>
2019-07-27 15:55:24 -0300

    removing multer from routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,19 +1,17 @@
 const express = require('express')
-const multer = require('multer')
 
 const ClientController = require('../controllers/ClientController')
 const TransactionController = require('../controllers/TransactionController')
 
 const routes = new express.Router()
-const upload = multer({ dest: './' })
 
 // Clients
 routes.get('/clients', ClientController.index)
-routes.post('/clients', upload.single('optmize'), ClientController.create)
+routes.post('/clients', ClientController.create)
 
 // Transactions
 routes.get('/transactions', TransactionController.index)
-routes.post('/transactions', upload.single('optmize'), TransactionController.create)
+routes.post('/transactions', TransactionController.create)
 
 // Customer Balance Inquiries
 routes.get('/clients/available/:id', TransactionController.balanceAvailable)
